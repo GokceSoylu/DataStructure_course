08143384604229b55c283855c5f4a995357356d6
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-10-20 21:06:58 +0300

    .

--- TeachersNotes/DoubleList/deleteNode.c
+++ TeachersNotes/DoubleList/deleteNode.c
@@ -1,4 +1,12 @@
-struct node*delete(srruct node *head, int x)
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *pre;
+};
+struct node *delete(struct node *head, int x)
 {
     if(head!=NULL)
     {
@@ -16,8 +24,46 @@ struct node*delete(srruct node *head, int x)
             tmp->pre->next=tmp->next;
             if(tmp->next!=NULL)
                 tmp->next->pre=tmp->pre;
-            
         }
     }
     return head;
+}
+void print(struct node* head)
+{
+    struct node *p=head;
+    while(p!=NULL)
+    {
+        printf("\n%d",p->data);
+        p=p->next;
+    }
+}
+int main()
+{
+    struct node *head=NULL, *tmp, *p=(struct node*)malloc(sizeof(struct node));
+    int i ,c;
+    printf("how many nodes are there ");
+    scanf("%d",&c);
+    for(i=0;i<c;i++)
+    {
+        if(head==NULL)
+        {
+            head=p;
+            p->pre=NULL;
+            scanf("%d",&p->data);
+        }
+        else
+        {
+            p->next=(struct node*)malloc(sizeof(struct node));
+            tmp=p;
+            p=p->next;
+            p->pre=tmp;
+            scanf("%d",&p->data);
+        }
+    }  
+    p->next=NULL;
+    print(head);
+    head=delete(head, 1);
+    printf("\n\nafter\n");
+    print(head);
+    return 0;
 }
