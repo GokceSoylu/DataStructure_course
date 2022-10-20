a434a12fbb04ab7e35e32ebc958ab1e5be3abcf2
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-10-20 21:16:12 +0300

    ,

--- TeachersNotes/DoubleList/DoubleLİnkedList.c
+++ TeachersNotes/DoubleList/DoubleLİnkedList.c
@@ -6,3 +6,39 @@ struct node
     int data;
     struct node *next;
 };
+void print(struct node *head)
+{
+    struct node *p=head;
+    while(p!=NULL)
+    {
+        printf("\n%d",p->data);
+        p=p->next;
+    }
+}
+int main()
+{
+    struct node *head=NULL, *tmp, *p=(struct node*)malloc(sizeof(struct node));
+    int i, c;
+    printf("how many dodes are there ");
+    scanf("%d",&c);
+    for(i=0;i<c;i++)
+    {
+        if(head==NULL)
+        {
+            head=p;
+            p->pre=NULL;
+            scanf("%d",&p->data);
+        }
+        else
+        {
+            p->next=(struct node*)malloc(sizeof(struct node));
+            tmp=p;
+            p=p->next;
+            p->pre=tmp;
+            scanf("%d",&p->data);
+        }
+    }
+    p->next=NULL;
+    print(head);
+    return 0;
+}
