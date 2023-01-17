c0b40f4c036cf310e449509113b11a5299504a6f
NecmiyeSoylu <necmiyesoyla@gmail.com>
2023-01-17 21:07:02 +0300

    priority queue min binary heap

--- PriorityQueue/PriortiyMain.c
+++ PriorityQueue/PriortiyMain.c
@@ -2,17 +2,16 @@
 int main()
 {
     int i,array[10]={0,1,5,6,20,0,0,0,0,0};
-    for(i=1;i<7;i++)
+    for(i=1;i<10;i++)
         printf("\t%d",array[i]);
     printf("\n");
-    insertToMin_(array,4,5);
-    for(i=1;i<7;i++)
+    insertToMin(array,4,5);
+    for(i=1;i<10;i++)
         printf("\t%d",array[i]);
     printf("\n");
-    printf("\narray[5] = %d\n",array[5]);
     printf("deleted %d\n",deleteToMin(array,6));
     printf("\n");
-    //for(i=1;i<5;i++)
-      //  printf("\t%d",array[i]);
+    for(i=1;i<10;i++)
+        printf("\t%d",array[i]);
     return 0;
 }
--- PriorityQueue/priortyQueue.h
+++ PriorityQueue/priortyQueue.h
@@ -2,12 +2,11 @@
 #define SIZE 10
 void swap(int *a,int *b)
 {
-    int tmp;
-    tmp=*a;
+    int tmp=*a;
     *a=*b;
     *b=tmp; 
 }
-void insertToMin_(int*array, int data,int i)
+void insertToMin(int*array, int data,int i)
 {
     if(i==SIZE)
         printf("the heap is full");
@@ -34,25 +33,35 @@ int deleteToMin(int* array, int index)
     else
     {
         //silinecek eleamnı koruyoruz
-        min=array[1];
+        min=array[i];
 
         //sondaki elemanı başa atıyoruz
-        array[1]=array[index-1];
+        array[i]=array[index-1];
         array[index-1]=0;
 
         //parent'ın child'dan küçük olmasını sağlıyoruz
-        while(array[i*2]<array[i] || array[(2*i)+1]<array[i])
+        while((i*2+1)<SIZE &&(array[i]>array[i*2] || array[i]>array[(2*i)+1]))
         {
             //hangisinin daha küçük olduğunu kontrol ediyoorz. kardeş kıyası
-            if(array[i*2]<array[i*2+1])
+            if(array[i*2]<array[(i*2)+1])
             {
-                swap(&array[i],&array[i*2]);
-                i=i*2;
+                //sıfır olup olmadığını kontrol ediyoruz sona ulamış boş kısma ulaşmış olabiliriz
+                if(array[i*2]!=0)
+                {
+                    swap(&array[i],&array[i*2]);
+                    i=i*2;
+                }
+                else break;
             }    
             else
             {
-                swap(&array[i],&array[i*2+1]);
-                i=i*2+1;
+                if(array[i*2+1]!=0)
+                {
+                    swap(&array[i],&array[i*2+1]);
+                    i=(i*2)+1;
+                }
+                else break;  
+                
             }   
         }
         return min;
