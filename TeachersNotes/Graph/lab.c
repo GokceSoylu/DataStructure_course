376fda5c4a934e5d09790c01864c7fd27b317f28
NecmiyeSoylu <necmiyesoyla@gmail.com>
2023-01-17 22:14:39 +0300

    graph lab section question

--- TeachersNotes/Graph/lab.c
+++ TeachersNotes/Graph/lab.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-int edges(int A[][5])
+int edges(int A[][5],int boyut )
 {
     int i,j, result=0;
-    for(i=0;i<5;i++)
-        for(j=0;j<5;j++)
+    for(i=0;i<boyut;i++)
+        for(j=0;j<boyut;j++)
             result+=A[i][j];
     return result;
 }
@@ -18,11 +18,12 @@ int main()
 {
     int n=5,i;
     int A[5][5]={{0,1,1,1,0},
-                {1,0,0,1,0},
-                {1,0,0,1,1},
-                {1,1,1,0,1},
-                {0,0,1,1,0}};
+                 {1,0,0,1,0},
+                 {1,0,0,1,1},
+                 {1,1,1,0,1},
+                 {0,0,1,1,0}};
     int degree_sum=0;
+    printf("\nedges = %d\n",edges(A,5));
     for(i=0;i<n;i++)
     {
         degree_sum+=degree(A,i,n);
