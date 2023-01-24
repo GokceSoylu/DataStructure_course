fef9531b8e51df0055ccf63f21ad9e0dbff2b865
NecmiyeSoylu <necmiyesoyla@gmail.com>
2023-01-24 22:16:15 +0300

    include

---
+++ convert_BTreeToAVL/BST.h
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct tree
+{
+    int data;
+    struct tree *left, *right;
+};
+typedef struct tree tree;
+typedef tree *BST;
+BST add(BST root, int data)// works👍🏻
+{
+    if(root==NULL)
+    {
+        BST root=(BST)malloc(sizeof(tree));
+        root->left=root->right=NULL;
+        root->data=data;
+        return root;
+    }    
+    if(root->data < data)
+    {
+        root->right=add(root->right, data);
+        return root;
+    }    
+    root->left=add(root->left, data);
+    return root;
+}
+void traverse_in(BST root)// works👍🏻
+{
+    if(root==NULL) return;
+    traverse_in(root->left);
+    printf("\n%d",root->data);
+    traverse_in(root->right);
+}
+void traverse_pre(BST root)// works👍🏻
+{
+    if(root==NULL) return;
+    printf("\n%d",root->data);
+    traverse_pre(root->left);
+    traverse_pre(root->right);
+}
+void traverse_pos(BST root)// works👍🏻
+{
+    if(root==NULL) return;
+    traverse_pos(root->left);
+    traverse_pos(root->right);
+    printf("\n%d",root->data);
+}
+int search_general(BST root, int data) // çok güzel oldu yaa. works 👍🏻
+{
+    if(root==NULL) return 0;
+    if(data==root->data) return 1;
+    if(search_general(root->right,data)==1) return 1;
+    if(search_general(root->left,data)==1)return 1;
+    return 0;
+}
+int search(BST root, int data)//works 👍🏻
+{
+    int x=0;
+    if(root==NULL) return 0;
+    if(root->data==data) return 1;
+    if(data<root->data) x=search(root->left,data);
+    else x=search(root->right,data);
+    return x;
+}
+int maxBST(BST root)
+{
+    while(root->right!=NULL) root=root->right;
+    return root->data;
+}
+int minBST(BST root)
+{
+    while(root->left!=NULL) root=root->left;
+    return root->data;
+}
+BST deleteBST(BST root,int data)
+{
+    if(root==NULL) return NULL;
+    if(root->data == data) 
+    {
+        if(root->right==NULL && root->left==NULL) return NULL;
+        if(root->right!=NULL)
+        {
+            root->data=minBST(root->right);
+            root->right=deleteBST(root->right,minBST(root->right));
+            return root;
+        }
+        root->data=maxBST(root->left);
+        root->left=deleteBST(root->left,maxBST(root->left));
+        return root; 
+    }
+    if(data<root->data)
+    {
+        deleteBST(root->left,data);
+        return root;
+    } 
+    root->right=deleteBST(root->right,data);
+    return root;
+}
